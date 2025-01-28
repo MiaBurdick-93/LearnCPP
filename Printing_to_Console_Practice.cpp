0678c8db9d0c774695d57969fb187b02a81eb152
MiaBurdick-93 <burdicmp@mail.uc.edu>
2025-01-28 10:25:17 -0500

    Add files via upload

---
+++ Printing_to_Console_Practice.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/*The purpose of this program is to create a haiku
+* to practice the concepts and associated syntax learned in week 1.
+*/
+int main() {
+	std::cout << "Spring follows Fall\n";
+	std::cout << "Or maybe Spring comes before\n";
+	std::cout << "Long days quickly transform to short years" << std::endl;
+	std::cout << "So does the difference even matter" << std::endl;
+	std::cout << "A haiku by Mia Burdick, 2025.";
+	return 0;
+}
